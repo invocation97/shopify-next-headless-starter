e5f68e8ce7148d2b69ef625844532de3a0b7163d
invocation97 <danilo97benovic@gmail.com>
2025-12-17 02:02:13 +0100

    Base tanstack setup (#2)

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,4 +1,8 @@
 const env = {
+    /* General */
+    NODE_ENV: process.env.NODE_ENV!,
+
+    /* Shopify */
     SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY!,
     SHOPIFY_API_SECRET: process.env.SHOPIFY_API_SECRET!,
     SHOPIFY_API_URL: process.env.SHOPIFY_API_URL!,
